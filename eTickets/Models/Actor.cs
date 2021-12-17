aa9a5633995738e9ee8c4aef71dcd0aae2ff4394
abhishek.upadhyaya <abhishek.upadhyaya@DEL1-LHP-N67688>
2021-12-17 10:58:26 +0530

    Add project files.

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        public string ImageURL { get; set; }
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
